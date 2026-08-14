f6c24b07ea3437452890f6be9974278d5a20704d
Shashwat Srivastava <shashwat.2428it2297@kiet.edu>
2026-08-14 18:46:58 +0530

    Time: 72 ms (5.44%) | Memory: 83.4 MB (5.73%) - LeetSync

---
+++ potd/72-edit-distance/edit-distance.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> dp;
+    int find(string s,string t,int i,int j){
+        if(i<0) return j+1;
+        if(j<0) return i+1;
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(s[i]==t[j]) return dp[i][j]=find(s,t,i-1,j-1);
+        int a=find(s,t,i,j-1);
+        int b=find(s,t,i-1,j);
+        int c=find(s,t,i-1,j-1);
+        return dp[i][j]=1+min({a,b,c});
+    }
+
+    int minDistance(string s, string t) {
+        int n=s.size(),m=t.size();
+        if(n==0 || m==0) return max(n,m);
+        dp.resize(n+1,vector<int>(m+1,-1));
+        if(s==t) return 0;
+        return find(s,t,n-1,m-1);
+    }
+};

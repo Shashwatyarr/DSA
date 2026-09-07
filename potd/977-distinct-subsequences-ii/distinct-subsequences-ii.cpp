5abf76b8594f3bb4cb1004721cda0c6f8e4b28d7
Shashwat Srivastava <shashwat.2428it2297@kiet.edu>
2026-09-07 17:36:39 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (43.73%) - LeetSync

---
+++ potd/977-distinct-subsequences-ii/distinct-subsequences-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> dp;
+    int MOD=1e9+7;
+    int find(string& s,unordered_map<char,int>& mpp, int i){
+        if(i==s.size()) return 1;
+        if(dp[i]!=-1) return dp[i];
+        long long take=find(s,mpp,i+1);
+        long long sum=((take*2)-mpp[s[i]]+MOD)%MOD;
+        mpp[s[i]]=take;
+        return dp[i]=sum;
+    }
+
+    int distinctSubseqII(string s) {
+        int n=s.size();
+        dp.resize(n,-1);
+        unordered_map<char,int> mpp;
+        for(auto it:s){
+            mpp[it]=0;
+        }
+        int ans=find(s,mpp,0);
+        return (ans-1+MOD)%MOD;
+    }
+};

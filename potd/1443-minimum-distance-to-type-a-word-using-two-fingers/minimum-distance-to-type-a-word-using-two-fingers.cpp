9224afd26b73739b5aea385d9c0c0b92a65bcb1d
Shashwat Srivastava <shashwat.2428it2297@kiet.edu>
2026-09-08 13:57:04 +0530

    Time: 51 ms (30.71%) | Memory: 48.6 MB (19.09%) - LeetSync

---
+++ potd/1443-minimum-distance-to-type-a-word-using-two-fingers/minimum-distance-to-type-a-word-using-two-fingers.cpp
@@ -0,0 +1,57 @@
+class Solution {
+    vector<vector<vector<int>>> dp; // i,h1,h2
+    char keyboard[5][6];
+    vector<pair<int, int>> mp;
+
+public:
+    int find(string& word, int i, int h1, int h2) {
+        if (i >= word.size())
+            return 0;
+
+        if (dp[i][h1][h2] != -1)
+            return dp[i][h1][h2];
+
+        int curr = word[i] - 'A';
+
+        int d1 = 0, d2 = 0;
+
+        if (h1 != 26)
+            d1 = abs(mp[h1].first - mp[curr].first) +
+                 abs(mp[h1].second - mp[curr].second);
+
+        if (h2 != 26)
+            d2 = abs(mp[h2].first - mp[curr].first) +
+                 abs(mp[h2].second - mp[curr].second);
+
+        return dp[i][h1][h2] =
+                   min((find(word, i + 1, curr, h2) + d1),
+                       (find(word, i + 1, h1, curr) + d2));
+    }
+
+    int minimumDistance(string word) {
+        int n = word.size();
+        char a = 'A';
+        int count = 0;
+
+        mp.clear();
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 6; j++) {
+                if (count == 26)
+                    break;
+
+                keyboard[i][j] = a;
+                mp.push_back({i, j});
+                a++;
+                count++;
+            }
+
+            if (count == 26)
+                break;
+        }
+
+        dp.resize(n, vector<vector<int>>(27, vector<int>(27, -1)));
+
+        return find(word, 0, 26, 26);
+    }
+};

c336257631abcc5e2572ba275a109665290963ae
Shashwat Srivastava <shashwat.2428it2297@kiet.edu>
2026-09-10 14:02:45 +0530

    Time: 2 ms (55.02%) | Memory: 9.1 MB (22.19%) - LeetSync

---
+++ potd/52-n-queens-ii/n-queens-ii.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int n;
+    vector<vector<string>> ans;
+    vector<string> board;
+    bool isS(int row,int col){
+        for (int i = 0; i < row; i++) {
+            if (board[i][col] == 'Q') return 0;
+        }
+        for(int i=row-1,j=col-1;i>=0 && j>=0 ;i--,j--){
+            if(board[i][j]=='Q') return 0;
+        }
+        for(int i=row-1,j=col+1;i>=0 && j<n;i--,j++){
+            if(board[i][j]=='Q') return 0;
+        }
+        return 1;
+     }
+
+    void solve(int row){
+        if(row==n) {
+            ans.push_back(board);
+            return ;
+        }
+        for(int i=0;i<n;i++){
+            if(isS(row,i)){
+                board[row][i]='Q';
+                solve(row+1);
+                board[row][i]='.';
+            }
+        }
+    }
+    int totalNQueens(int a) {
+         n=a;
+        board.assign(n,string(n,'.'));
+        solve(0);
+        return ans.size();
+    }
+};  

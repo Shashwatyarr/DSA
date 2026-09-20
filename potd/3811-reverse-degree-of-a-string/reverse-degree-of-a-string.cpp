2920ccd5a6c8860def227d987c728155e14fc658
Shashwat Srivastava <shashwat.2428it2297@kiet.edu>
2026-09-20 10:19:28 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (47.88%) - LeetSync

---
+++ potd/3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int reverseDegree(string s) {
+        int ans=0;
+        int n=s.size();
+        for(int i=0; i<n; i++) ans+=(i+1)*('z'-s[i]+1);
+        return ans;
+    }
+};
